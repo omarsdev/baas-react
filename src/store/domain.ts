bcf10dce678f2a471d8bd6ac97224293bc34047d
Omar Sukarieh <omarskdev@gmail.com>
2023-02-15 02:19:27 +0300

    fix the initDomain

--- src/store/domain.ts
+++ src/store/domain.ts
@@ -33,11 +33,11 @@ export const initDomain = (props: initDomainPropsTypes) => {
   let newData: newDomainStorePropTypes = { ...props }
 
   if (props.connectToProd !== undefined) {
-    newData = { ...props, urlConnection: props.connectToProd ? URL_PROD : URL_QA }
+    newData = { ...newData, urlConnection: props.connectToProd ? URL_PROD : URL_QA }
   }
 
   if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    newData = { ...props, isSSR: true }
+    newData = { ...newData, isSSR: true }
   }
 
   useDomainStore.setState(newData)
