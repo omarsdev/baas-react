e931a62f1e1c721cd8d1d2c62a5053deeaa9945a
Omar Sukarieh <omarskdev@gmail.com>
2023-03-15 00:20:54 +0300

    add keyQuery

--- src/api/sendForm/index.ts
+++ src/api/sendForm/index.ts
@@ -2,18 +2,26 @@ import axios from 'axios'
 
 import { useDomainStore } from '../../store/domain'
 
-export const sendForm = async (body: object) => {
+interface sendFormPropsTypes {
+  body: object
+  key?: string
+}
+
+export const sendForm = async (props: sendFormPropsTypes) => {
+  const { body, key } = props
+
   const urlConnection = useDomainStore.getState().urlConnection
   const domain = useDomainStore.getState().domain
 
   const result = { data: null, error: null }
+  const keyQuery = key ? `&key=${key}` : ''
 
   if (!domain) {
     throw new Error(`Domain need to be set first, Please setup your domain first by using initDomain()`)
   }
 
   await axios
-    .post(`${urlConnection}/contact-us?domainName=${domain}`, body)
+    .post(`${urlConnection}/contact-us?domainName=${domain}${keyQuery}`, body)
     .then((response) => {
       result.data = response.data.contactMessage
     })
